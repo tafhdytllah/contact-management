7c66db6e59923f8eabef4a32bad59c189fbbd712
tafhdytllah <taufikhh.97@gmail.com>
2024-03-18 23:55:03 +0700

    create table and entity User and Contact

--- database.sql
+++ database.sql
@@ -2,3 +2,34 @@
 CREATE DATABASE spring_restful;
 
 USE spring_restful;
+
+CREATE TABLE users(
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    token VARCHAR(100),
+    token_expired_at BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+) ENGINE InnoDB;
+
+DROP TABLE users;
+
+SELECT * FROM users;
+
+DESC users;
+
+CREATE TABLE contacts(
+    id VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100),
+    phone VARCHAR(100),
+    email VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_users_contacts (username) REFERENCES users (username)
+) ENGINE InnoDB;
+
+SELECT * FROM contacts;
+
+DESC contacts;
